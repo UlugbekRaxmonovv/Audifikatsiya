011623911208781a37305317882c8326bfcdeba9
Ulug'bek <rahmonovulugbek20042004@gmail.com>
2024-11-12 15:04:45 +0500

    vercel.jsons

--- src/components/Audifikatsiya.jsx
+++ src/components/Audifikatsiya.jsx
@@ -2,14 +2,13 @@ import { initializeApp, getApps} from "firebase/app";
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmne0JnJ0GlAVAJTahHSyKsn9s2mq1oF0",
-    authDomain: "audifakatsiya.firebaseapp.com",
-    projectId: "audifakatsiya",
-    storageBucket: "audifakatsiya.appspot.com",
-    messagingSenderId: "826403984823",
-    appId: "1:826403984823:web:e6db4d44138a6437000432",
+    apiKey: "AIzaSyA8KiOGUurOyJb9zKU3g01Q9BRCWrytY_8",
+    authDomain: "login-b1e67.firebaseapp.com",
+    projectId: "login-b1e67",
+    storageBucket: "login-b1e67.firebasestorage.app",
+    messagingSenderId: "490453755881",
+    appId: "1:490453755881:web:8c2db9dfeed4b562d94d78"
   };
-  
   const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
   
   const auth = getAuth(app);
